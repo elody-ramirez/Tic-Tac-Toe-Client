f63d04500eeabb59c338cc7db67652d5e775d573
elody-ramirez <elody.ramirez@gmail.com>
2019-06-26 12:07:09 -0400

    Add SignIn functionality
    Started with the curl script for sign in to make sure API was working
    properly. Then added the sign in form in html. Added event listener,
    event handler, api and ui for sign in.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -17,14 +17,24 @@ const failureMessage = message => {
 }
 
 const signUpSuccessful = responseData => {
-  successMessage('You signed up Successfully PAL!')
+  successMessage('You signed up successfully!')
 }
 
 const signUpFailure = () => {
-  failureMessage('You failed to sign up PAL!')
+  failureMessage('You failed to sign up!')
+}
+
+const signInSuccessful = responseData => {
+  successMessage('You logged into your account!')
+}
+
+const signInFailure = () => {
+  failureMessage("You weren't able to log in!")
 }
 
 module.exports = {
   signUpSuccessful,
-  signUpFailure
+  signUpFailure,
+  signInSuccessful,
+  signInFailure
 }
